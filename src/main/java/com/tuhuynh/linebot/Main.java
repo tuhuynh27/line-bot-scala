e3cea5197ae6534d1647723342c9d106881a7107
Tu Huynh <tu.huynh@linecorp.com>
2020-10-02 17:51:32 +0700

    added shutdown hook

--- src/main/java/com/tuhuynh/linebot/Main.java
+++ src/main/java/com/tuhuynh/linebot/Main.java
@@ -3,11 +3,13 @@ package com.tuhuynh.linebot;
 import com.jinyframework.HttpServer;
 import com.jinyframework.core.RequestBinderBase.HttpResponse;
 import com.tuhuynh.linebot.handlers.WebhookHandler;
+import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 
 import java.io.IOException;
 import java.util.Map;
 
+@Slf4j
 public final class Main {
     public static void main(String[] args) throws IOException {
         final Map<String, String> env = System.getenv();
@@ -19,6 +21,16 @@ public final class Main {
         final String port = env.get("PORT") == null ? "1234" : env.get("PORT");
 
         val server = HttpServer.port(Integer.parseInt(port));
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                server.stop();
+            } catch (IOException e) {
+                log.error(e.getMessage(), e);
+            }
+        }));
+        log.info("Added shutdown hook");
+
         val webhookHandler = new WebhookHandler(token);
         server.post("/webhook", webhookHandler::handleWebhook);
         server.get("/dict", webhookHandler::showDict);
