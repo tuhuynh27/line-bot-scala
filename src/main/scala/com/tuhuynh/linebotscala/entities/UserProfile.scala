612de54b7985ee379aa781ce8b5e09a7caaf5145
Tu Huynh <tu.huynh@linecorp.com>
2021-03-30 16:38:08 +0700

    continue migrate

---
+++ src/main/scala/com/tuhuynh/linebotscala/entities/UserProfile.scala
@@ -0,0 +1,3 @@
+package com.tuhuynh.linebotscala.entities
+
+case class UserProfile(displayName: String, userId: String, pictureUrl: String, statusMessage: String)
--- src/main/scala/com/tuhuynh/linebotscala/handlers/WebhookHandler.scala
+++ src/main/scala/com/tuhuynh/linebotscala/handlers/WebhookHandler.scala
@@ -2,7 +2,7 @@ package com.tuhuynh.linebotscala.handlers
 
 import com.google.gson.Gson
 import com.jinyframework.HttpClient
-import com.tuhuynh.linebotscala.entities.{Event, Messages, ReplyObject, WebhookEventObject}
+import com.tuhuynh.linebotscala.entities.{Event, Messages, ReplyObject, UserProfile, WebhookEventObject}
 
 import java.util
 import scala.collection.mutable
@@ -16,6 +16,19 @@ class WebhookHandler(val token: String) {
     webhookEventObject.events(0)
   }
 
+  def getProfile(event: Event): UserProfile = {
+    val headers = new util.HashMap[String, String]
+    headers.put("Authorization", "Bearer " + token)
+    val response = HttpClient.builder
+      .method("GET").url("https://api.line.me/v2/bot/profile/" + event.source.userId)
+      .headers(headers)
+      .build.perform
+    if (response.getStatus == 200) {
+      return gson.fromJson(response.getBody, UserProfile.getClass)
+    }
+    throw new Exception("Cannot get user profile")
+  }
+
   def reply(event: Event, text: String): Unit = {
     val headers = new util.HashMap[String, String]
     headers.put("Content-Type", "application/json")
@@ -23,8 +36,8 @@ class WebhookHandler(val token: String) {
     val message = Messages("text", text)
     val replyObject = ReplyObject(event.replyToken, Array(message))
     val replyObjectJson = gson.toJson(replyObject)
-    HttpClient.builder().method("POST").url("https://api.line.me/v2/bot/message/reply")
+    HttpClient.builder.method("POST").url("https://api.line.me/v2/bot/message/reply")
       .headers(headers).body(replyObjectJson)
-      .build().perform()
+      .build.perform
   }
 }
